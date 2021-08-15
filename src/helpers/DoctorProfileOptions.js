116b7af8ea7e7122dbeb22c21a6c2d729e2485e4
Rat <ratulbhowmick65@gmail.com>
2021-08-16 03:16:18 +0600

    last update

---
+++ src/helpers/DoctorProfileOptions.js
@@ -0,0 +1,7 @@
+const DoctorProfileOptions = [
+  {title: 'History', key: '1', navTo: 'DocHistory'},
+  {title: 'Appointments', key: '2', navTo: 'AppointMents'},
+  {title: 'Revenue', key: '3', navTo: 'DoctorRevenue'},
+];
+
+export default DoctorProfileOptions;
